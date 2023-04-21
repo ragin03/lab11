6af39e1668cdfd28608d0e503d3d5c323e189727
ragin03 <username@mailserver.com>
2023-04-21 15:03:15 -0700

    Fixed Constructor

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,10 @@
 package trees;
 
-public class TreeException extends ???
+public class TreeException extends Throwable
 {
 	public TreeException(String s)
 	{
+		super(s);
 		// Pass s into a superclass ctor.
 	}
 }
